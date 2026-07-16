c2e9f8e4c3f275a0336c5b3914bc3a4593306341
zhanghongjun <zhanghongjun@globalsphere.tech>
2026-07-16 13:10:01 +0800

    chore(release): basic train v0.4.19

--- tools/protoc-gen/typescript/src/main.ts
+++ tools/protoc-gen/typescript/src/main.ts
@@ -56,7 +56,7 @@ type LocalServiceMetadata = {
   methods: MethodMetadata[];
 };
 
-const VERSION = "0.4.18";
+const VERSION = "0.4.19";
 
 const plugin = createEcmaScriptPlugin<PluginParams>({
   name: "protoc-gen-actrframework-typescript",
